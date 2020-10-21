7aeba2e634c4ad5a988e00c6c8117a1f9d9b3946
RadarSun <hitsunzhicong@163.com>
2020-10-22 00:22:41 +0800

    add algorithm which use density to init centers

--- w5_kmeans_initial.py
+++ w5_kmeans_initial.py
@@ -45,6 +45,46 @@ from sklearn.cluster import KMeans
 from sklearn import metrics
 from matplotlib import animation
 from PIL import Image
+import matplotlib.patches as mpatches
+from math import pi
+from numpy import cos, sin
+
+def cal_nextcenter(density,distance_matrix,centers,pre_siteid_of_center):
+    distance_all_sites = []
+    n_sites = len(density)
+    n_centers = len(centers)
+    for i in range(n_sites):
+        total_distance = 1
+        for j in range(n_centers):
+            total_distance = total_distance * distance_matrix[i][pre_siteid_of_center[j]]
+        distance_all_sites.append(total_distance)
+        if density[i] == 0:
+            distance_all_sites[i] = 0
+    next_center_id = distance_all_sites.index(max(distance_all_sites))
+    return next_center_id
+
+def cal_distance(sites):
+    distance_list = []
+    n_sites = len(sites)
+    distance_matrix = np.zeros((n_sites,n_sites))
+    for i in range(n_sites):
+        for j in range(n_sites):
+            distance_matrix[i][j] = ( ((sites[i].x_location - sites[j].x_location) **2)+((sites[i].y_location - sites[j].y_location) **2) ) **0.5
+            distance_matrix[j][i] = distance_matrix[i][j]
+            distance_list.append(distance_matrix[i][j])
+    return distance_matrix,distance_list
+
+def cal_density(sites,distance_matrix,radius):
+    density = []
+    n_sites = len(sites)
+    for i in range(n_sites):
+        cnt_site = 0
+        for j in range(n_sites):
+            if distance_matrix[i][j] < radius:
+                cnt_site = cnt_site + 1
+        density.append(cnt_site)
+    return density
+
 
 def assign_center(centers,site):
     min_distance = [( ((site.x_location-centers[0].x_location) ** 2) + ((site.y_location-centers[0].y_location)**2) ) ** 0.5,0]
@@ -69,7 +109,7 @@ def cal_center(id,sites):
     center.y_location = sum_y/cnt_sites
     return center
 
-def k_means(sites, init_centers):
+def k_means(sites, init_centers,algorithm_kind):
     centers = init_centers[:]
     n_fig = 0
     while True:
@@ -101,7 +141,6 @@ def k_means(sites, init_centers):
                 if sites[i].center == j:
                     x_sample_location.append(sites[i].x_location)
                     y_sample_location.append(sites[i].y_location)  
-            print(len(x_sample_location))
             plt.scatter(x_sample_location,y_sample_location,marker='o',c = color_squence[j%7])
 
         x_center_location = []
@@ -110,12 +149,15 @@ def k_means(sites, init_centers):
             x_center_location.append(centers[i].x_location)
             y_center_location.append(centers[i].y_location)    
         plt.scatter(x_center_location,y_center_location,s=400,marker='*',c='red')
-        plt.savefig(str(n_fig)+'.png')
+        plt.title('Init by '+ algorithm_kind)
+        plt.xlabel('Number of iterations:' + str(n_fig+1))
+        plt.savefig(str(algorithm_kind)+ '_' + str(n_fig)+'.png')
         n_fig = n_fig+1
-        plt.pause(1)
+        plt.pause(0.5)
 
 class Site:
     center = 0
+    # density = 0
     def __init__(self,id,x_location,y_location):
         self.id = id
         self.x_location = x_location
@@ -129,18 +171,21 @@ class Center:
         self.y_location = y_location
 
 if __name__ == "__main__":
+
     '''
-    Initial sample sites and initial centers
-    '''
-    # define super param
-    CENTERS=[[-1,1], [-1,-1], [1,1], [1,-1],[2,2]]
+    Define the clusters using super param
+    '''   
+    CENTERS=[[-1,1], [-1,-2], [1,1], [1,-1],[2,2]]
     # CENTERS=[[-1,-1], [-1,1]]
     K = len(CENTERS)
     N_SAMPLES = 300 # numbel of samples, K samples is used for initial centers
-    CLUSTER_STD = [0.5, 0.5, 0.5, 0.5, 0.4] # std of each cluster
+    CLUSTER_STD = [0.8, 0.8, 0.5, 0.5, 0.8] # std of each cluster
     # CLUSTER_STD = [0.2, 0.2]
-    
-    # inital sample sites
+
+
+    '''
+    Initial sample sites
+    '''
     Object_sites = []
     sample_sites_locations = []
     temp_sample_sites_locations, cluster_id = make_blobs(n_samples=N_SAMPLES, n_features=2, centers = CENTERS, cluster_std=CLUSTER_STD, random_state =9)
@@ -154,13 +199,17 @@ if __name__ == "__main__":
         sample_sites_locations.append([x_sample_location[n_sample],y_sample_location[n_sample]])
         Object_sites.append( Site(n_sample,sample_sites_locations[n_sample][0],sample_sites_locations[n_sample][1]) )
 
-    # inital centers
+    '''
+    Inital centers
+    '''
     Object_centers = []
     initial_centers_locations= []
     x_center_location = []
     y_center_location = []
+
+    # initial centers randomly
+    algorithm_kind = 'Random Initialization '
     rand_squence = np.random.randint(0,N_SAMPLES,K) # choose sites randomly
-    
     for k in range(K):
         # choose sites as centers from samples
         x_center_location.append(x_sample_location[rand_squence[k]])
@@ -174,25 +223,96 @@ if __name__ == "__main__":
         initial_centers_locations.append([x_center_location[k],y_center_location[k]])
         Object_centers.append( Center(k,initial_centers_locations[k][0],initial_centers_locations[k][1]) )
 
-    plt.ion()
+    fig = plt.figure(figsize=(5,5))
     plt.scatter(x_sample_location,y_sample_location, marker='o') # the sites before k-means
-    plt.scatter(x_center_location,y_center_location,s = 300,marker='*')
+    plt.scatter(x_center_location,y_center_location,s = 300,marker='*',c = 'red')
+    plt.title('Init by '+ algorithm_kind)
+    plt.savefig('inital_centers_Random.png')
 
     '''
-    K-Means
+    K-Means and plt.show
     '''
-    [Object_centers,n_fig] = k_means(Object_sites, Object_centers)
+    plt.ion()
+    [Object_centers,n_fig] = k_means(Object_sites, Object_centers,algorithm_kind)
     plt.ioff()
     plt.show()
 
-
-    im = Image.open("0.png")
+    '''
+    Save figs as gif
+    '''
+    im = Image.open(str(algorithm_kind) + "_0.png")
     images=[]
     for i in range(n_fig):
         if i!=0:
-            fpath = str(i) + ".png"
+            fpath = str(algorithm_kind) + '_' + str(i) + ".png"
             images.append(Image.open(fpath))
-    im.save('kmeans.gif', save_all=True, append_images=images,loop=2000,duration=1)
+    im.save(str(algorithm_kind) + '.gif', save_all=True, append_images=images,loop=1000,duration=500)
+
+
+    #############################################################################
+    Object_centers = []
+    initial_centers_locations= []
+    x_center_location = []
+    y_center_location = []
+
+    # iniital centers using density
+    algorithm_kind = 'DBCIM'
+    [distance_matrix,distance_list] = cal_distance(Object_sites)
+    radius = 0.5 * max(distance_list) / K
+    density = cal_density(Object_sites,distance_matrix,radius)
+    mindoc = sum(density) / len(density)
+
+    for i in range(len(density)):
+        if density[i] < mindoc:
+            density[i] = 0
+    pre_siteid_of_center = []
+    pre_siteid_of_center.append(density.index(max(density)))
+    x_center_location.append(x_sample_location[pre_siteid_of_center[0]])
+    y_center_location.append(y_sample_location[pre_siteid_of_center[0]])
+    initial_centers_locations.append([x_center_location[0],y_center_location[0]])
+    Object_centers.append( Center(0,x_center_location[0],y_center_location[0]))
+    # plot the initial centers and sites
+    angles_circle = [i * pi / 180 for i in range(0, 360)]  # i change to double
+    x = cos(angles_circle)
+    y = sin(angles_circle)
+    fig = plt.figure(figsize=(5,5))
+    plt.scatter(x_sample_location,y_sample_location, marker='o') # the sites before k-means
+    plt.scatter(x_center_location,y_center_location,s = 300,marker='*',c = 'red')
+    plt.plot(x+x_center_location[0],y+y_center_location[0],'r')
+
+    for i in range(K-1):
+        pre_siteid_of_center.append(cal_nextcenter(density,distance_matrix,Object_centers,pre_siteid_of_center))
+        x_center_location.append(x_sample_location[pre_siteid_of_center[i+1]])
+        y_center_location.append(y_sample_location[pre_siteid_of_center[i+1]])
+        initial_centers_locations.append([x_center_location[i+1],y_center_location[i+1]])
+        Object_centers.append( Center(i+1,x_center_location[i+1],y_center_location[i+1]))
+
+        # # plot the initial centers and sites
+        plt.scatter(x_sample_location,y_sample_location, marker='o') # the sites before k-means
+        plt.scatter(x_center_location,y_center_location,s = 300,marker='*',c = 'red')
+        plt.plot(x+x_center_location[i+1],y+y_center_location[i+1],'r')
+          
+    plt.title('Init by '+ algorithm_kind)
+    plt.savefig('inital_centers_DBCIM.png')
+
+    '''
+    K-Means and plt.show
+    '''
+    plt.ion()
+    [Object_centers,n_fig] = k_means(Object_sites, Object_centers,algorithm_kind)
+    plt.ioff()
+    plt.show()
+
+    '''
+    Save figs as gif
+    '''
+    im = Image.open(str(algorithm_kind) + "_0.png")
+    images=[]
+    for i in range(n_fig):
+        if i!=0:
+            fpath = str(algorithm_kind) + '_' + str(i) + ".png"
+            images.append(Image.open(fpath))
+    im.save(str(algorithm_kind) + '.gif', save_all=True, append_images=images,loop=1000,duration=500)
     
 
 
